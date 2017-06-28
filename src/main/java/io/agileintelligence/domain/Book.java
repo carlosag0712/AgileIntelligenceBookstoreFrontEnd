f25ab739fce2a7203686e1a7d97015244caebe05
carlos arosemena <ceag68@hotmail.com>
2017-06-28 15:28:11 -0400

    update image url

--- src/main/java/io/agileintelligence/domain/Book.java
+++ src/main/java/io/agileintelligence/domain/Book.java
@@ -27,6 +27,7 @@ public class Book {
     private double listPrice;
     private double ourPrice;
     private boolean active=true;
+    private String imageUrl;
 
 
     @Column(columnDefinition = "text")
@@ -171,5 +172,13 @@ public class Book {
 
     public void setBookImage(MultipartFile bookImage) {
         this.bookImage = bookImage;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
